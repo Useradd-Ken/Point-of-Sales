d2e4b8bc02a99ad36669f996f5d66a9bdee2d541
Kenrawr <20102188@usc.edu.ph>
2026-06-16 18:24:27 +0800

    scaffolding ( React + js )

---
+++ app/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1 className="text-lg text-black/30">hello world</h1>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
